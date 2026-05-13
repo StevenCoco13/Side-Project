53bebdbc29b45f100a8b9b01f9fea10f90a04610
StevenCoco13 <steven1997336@gmail.com>
2026-05-13 21:01:17 +0800

    Update side.py

--- side.py
+++ side.py
@@ -1,3 +1,11 @@
 import streamlit as st
 import google.generativeai as genai
 
+import streamlit as st
+# 頁面初始化設定
+st.set_page_config(
+page_title="中科 AI 客服 - 闕老師實戰班",
+page_icon=" ",
+layout="wide" # "wide" 可利用全螢幕寬度,適合放置儀表板
+)
+
