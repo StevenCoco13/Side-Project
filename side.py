f6ed5a0a576d00b0f81b524ec4f98e02383e51bd
StevenCoco13 <steven1997336@gmail.com>
2026-05-13 21:39:37 +0800

    Update side.py

--- side.py
+++ side.py
@@ -46,25 +46,7 @@ st.metric(label="2026, 5, 13", value="9:30", delta="PM")
 
 
 
-def get_user_name():
-    return 'John'
 
-with st.echo():
-    # Everything inside this block will be both printed to the screen
-    # and executed.
-
-    def get_punctuation():
-        return '!!!'
-
-    greeting = "Hi there, "
-    value = get_user_name()
-    punctuation = get_punctuation()
-
-    st.write(greeting, value, punctuation)
-
-# And now we're back to _not_ printing to the screen
-foo = 'bar'
-st.write('Done!')
 
 
 
